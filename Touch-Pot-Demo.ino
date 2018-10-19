df88c391b61a51dc318d16cc15a03be3048a7999
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2018-10-19 14:23:26 -0600

    Added Large 7-segment to Touch Pot code

--- Touch-Pot-Demo.ino
+++ Touch-Pot-Demo.ino
@@ -25,6 +25,18 @@ int i2cAddr = 8; // Direct access at i2cAddr, indirect registers at i2cAddr+1
 uint8_t prevValue;
 uint8_t curValue;
 
+//Large 7-Segment stuff...
+//GPIO declarations
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+byte segmentClock = 6;
+byte segmentLatch = 5;
+byte segmentData = 7;
+byte addButton = 10;
+byte subtractButton = 11;
+
+int counter = 0;
+
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
@@ -37,6 +49,18 @@ void setup() {
   // Set Arduino LED PWM to match
   analogWrite(13, curValue);
   prevValue = curValue;
+
+  //Large 7-Segment Setup...
+  pinMode(segmentClock, OUTPUT);
+  pinMode(segmentData, OUTPUT);
+  pinMode(segmentLatch, OUTPUT);
+  pinMode(addButton,INPUT_PULLUP);
+  pinMode(subtractButton,INPUT_PULLUP);
+
+  digitalWrite(segmentClock, LOW);
+  digitalWrite(segmentData, LOW);
+  digitalWrite(segmentLatch, LOW);
+
 }
 
 void loop() {
@@ -46,9 +70,19 @@ void loop() {
   curValue = ReadTpValue(); // faster I2C access than register read
   if (curValue != prevValue) {
     analogWrite(13, curValue);
-    Serial.println(curValue);
+    Serial.print("curValue = ");
+    Serial.print(curValue);
     prevValue = curValue;
+    counter = map(curValue, 0, 255, 0, 9);
+    Serial.print("  counter = ");
+    Serial.println(counter);
+    postNumber(counter, false); //Show decimal
+    digitalWrite(segmentLatch, LOW);
+    digitalWrite(segmentLatch, HIGH);
   }
+  
+  
+  
 }
 
 // Write a Touch Potentiometer register
@@ -84,3 +118,75 @@ uint8_t ReadTpReg(uint8_t addr) {
     return 0;
   }
 }
+
+
+//Takes a number and displays 2 numbers. Displays absolute value (no negatives)
+void showNumber(float value)
+{
+  int number = abs(value); //Remove negative signs and any decimals
+
+  //Serial.print("number: ");
+  //Serial.println(number);
+
+  for (byte x = 0 ; x < 2 ; x++)
+  {
+    int remainder = number % 10;
+
+    postNumber(remainder, false);
+
+    number /= 10;
+  }
+
+  //Latch the current segment data
+  digitalWrite(segmentLatch, LOW);
+  digitalWrite(segmentLatch, HIGH); //Register moves storage register on the rising edge of RCK
+}
+
+//Given a number, or '-', shifts it out to the display
+void postNumber(byte number, boolean decimal)
+{
+  //    -  A
+  //   / / F/B
+  //    -  G
+  //   / / E/C
+  //    -. D/DP
+
+#define a  1<<0
+#define b  1<<6
+#define c  1<<5
+#define d  1<<4
+#define e  1<<3
+#define f  1<<1
+#define g  1<<2
+#define dp 1<<7
+
+  byte segments;
+
+  switch (number)
+  {
+    case 1: segments = b | c; break;
+    case 2: segments = a | b | d | e | g; break;
+    case 3: segments = a | b | c | d | g; break;
+    case 4: segments = f | g | b | c; break;
+    case 5: segments = a | f | g | c | d; break;
+    case 6: segments = a | f | g | e | c | d; break;
+    case 7: segments = a | b | c; break;
+    case 8: segments = a | b | c | d | e | f | g; break;
+    case 9: segments = a | b | c | d | f | g; break;
+    case 0: segments = a | b | c | d | e | f; break;
+    case ' ': segments = 0; break;
+    case 'c': segments = g | e | d; break;
+    case '-': segments = g; break;
+  }
+
+  if (decimal) segments |= dp;
+
+  //Clock these bits out to the drivers
+  for (byte x = 0 ; x < 8 ; x++)
+  {
+    digitalWrite(segmentClock, LOW);
+    digitalWrite(segmentData, segments & 1 << (7 - x));
+    digitalWrite(segmentClock, HIGH); //Data transfers to the register on the rising edge of SRCK
+  }
+}
+
